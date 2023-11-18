f88c43f95f59440e4e991db29a93c2ee036d0a87
g1ftmkyelu <mkyelugift@gmail.com>
2023-11-18 05:20:28 +0200

    update backend

--- backend/resources/medications.js
+++ backend/resources/medications.js
@@ -1,17 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const medicationSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        dosage: {
-            type: String,
-            required: true,
-        },
-    }
-);
+const medicationSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    dosage: {
+        type: String,
+        required: true,
+    },
+    quantityInStock: {
+        type: Number,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+});
 
 module.exports = { name: "Medication", endpoint: 'medications', schema: medicationSchema };
